6f1a991a1000f137536c8eab6450b78b2c9feb54
ANDY <68026794+JinchuanL@users.noreply.github.com>
2023-01-19 17:01:33 +1100

    NumberFormatException Exercise

---
+++ src/site/jinchuan/Ex2_98.java
@@ -0,0 +1,14 @@
+package site.jinchuan;
+
+// NumberFormatException Exercise
+public class Ex2_98
+{
+    public static void main(String[] args)
+    {
+        double a = 0.00;
+        String s = "HelloWorld";
+
+        a = Double.parseDouble(s);
+
+    }
+}
