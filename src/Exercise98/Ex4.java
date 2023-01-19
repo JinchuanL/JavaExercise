757cfe9162b9fe83d479417db2651702e589b39c
ANDY <68026794+JinchuanL@users.noreply.github.com>
2023-01-19 17:54:10 +1100

    Message

--- src/Exercise98/Ex4.java
+++ src/Exercise98/Ex4.java
@@ -1,6 +1,31 @@
 package Exercise98;
 
+// int overflow exception
+class Number
+{
+    int count(int a, int b)
+    {
+        return a*b;
+    }
+}
+
 public class Ex4
 {
+    public static void main(String[] args)
+    {
+        int a = 12315;
+        int b = 57876876;
 
+        Number n = new Number();
+
+        try
+        {
+            System.out.println(n.count(a, b));
+            Math.multiplyExact(a,b);
+        }
+        catch (ArithmeticException e)
+        {
+            System.out.println(e.toString());
+        }
+    }
 }
